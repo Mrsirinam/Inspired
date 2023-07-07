9cd13c9d88ff731e420ac085d3f48a73026a5c34
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-07-08 00:42:18 +0300

    fixed bag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { Navigation } from "./Navigation/Navigation";
 import { Top } from "./Top/Top";
 
-export const Header = (list) => (
+export const Header = ({ list }) => (
   <header>
     <Top />
     <Navigation list={list} />
