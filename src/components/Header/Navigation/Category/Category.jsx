88ab82dd99f594218934e31769a5b0acf0518404
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-07-10 23:09:26 +0300

    fix bag

--- src/components/Header/Navigation/Category/Category.jsx
+++ src/components/Header/Navigation/Category/Category.jsx
@@ -1,64 +1,29 @@
-import { NavLink, useLocation, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import s from "./Category.module.scss";
-import cn from "classnames";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import cn from "classnames";
+// import { createNextState } from "@reduxjs/toolkit";
 
-export const Category = () => {
-  const { activeGender: gender, categoryList } = useSelector(
-    (state) => state.navigation
-  );
+export const Category = ({ list }) => {
+  const { gender } = useSelector((state) => state.navigation.activeGender);
+  const categoriesList = list.find((item) => item.link === gender);
+  console.log(categoriesList, gender);
+
   return (
-    <>
-      {categoryList?.map((e, i) => (
-        <span key={i}>
-          {e[0] === gender && (
-            <div className={s.category}>
-              {e[1].list?.map((el, i) => (
-                <NavLink
-                  key={i}
-                  className={({ isActive }) =>
-                    cn(s.link, isActive && s.linkActive)
-                  }
-                  to={`catalog/${e[0]}/${el.slug}`}
-                >
-                  {el.title}
-                </NavLink>
-              ))}
-            </div>
-          )}
-        </span>
+    <ul className={s.category}>
+      {categoriesList?.categories.map((item) => (
+        <li key={item.link} className={s.item}>
+          <NavLink
+            className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
+            to={`${gender}/${item.link}`}
+          >
+            {item.title}
+          </NavLink>
+        </li>
       ))}
-    </>
+    </ul>
   );
 };
-
-// import { NavLink } from "react-router-dom";
-// import s from "./Category.module.scss";
-// import { useSelector } from "react-redux";
-// import cn from "classnames";
-// // import { createNextState } from "@reduxjs/toolkit";
-
-// export const Category = ({ list }) => {
-//   const { gender } = useSelector((state) => state.navigation.activeGender);
-//   const categoriesList = list.find((item) => item.link === gender);
-//   console.log(categoriesList, gender);
-
-//   return (
-//     <ul className={s.category}>
-//       {categoriesList.categories.map((item) => (
-//         <li key={item.link} className={s.item}>
-//           <NavLink
-//             className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
-//             to={`${gender}/${item.link}`}
-//           >
-//             {item.title}
-//           </NavLink>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
 
 // <>
 //   {list.map((e, i) => (
