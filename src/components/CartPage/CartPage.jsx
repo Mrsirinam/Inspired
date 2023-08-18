738b5f77cef2a36267a00af48768c07d63afd558
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-08-19 00:18:26 +0300

    fix bags

--- src/components/CartPage/CartPage.jsx
+++ src/components/CartPage/CartPage.jsx
@@ -1,15 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Cart } from "./Cart/Cart.jsx";
 import { Order } from "./Order/Order.jsx";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAll } from "../../features/goodsSlice.js";
 import { OrderModal } from "./OrderModal/OrderModal.jsx";
+import { Preloader } from "../Preloader/Preloader.jsx";
 
 export const CartPage = () => {
   const { cartItems, countItems } = useSelector((state) => state.cart);
-  const { goodsList } = useSelector((state) => state.goods);
+  const { goodsList, status } = useSelector((state) => state.goods);
   const { orderStatus } = useSelector((state) => state.cart);
+
   const [count, setCount] = useState(0);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,10 +20,12 @@ export const CartPage = () => {
     }
   }, [cartItems, count, countItems, dispatch]);
 
-  return (
+  return status === "loading" ? (
+    <Preloader />
+  ) : (
     <>
       <Cart cartItems={cartItems} goodsList={goodsList} />
-      {goodsList.length ? <Order cartItems={cartItems} /> : ""}
+      {!!cartItems.length && <Order cartItems={cartItems} />}
       {orderStatus === "success" && <OrderModal />}
     </>
   );
--- src/features/goodsSlice.js
+++ src/features/goodsSlice.js
@@ -60,6 +60,7 @@ const goodsSlice = createSlice({
     builder
       .addCase(fetchGender.pending, (state) => {
         state.status = "loading";
+        state.totalCount = null;
       })
       .addCase(fetchGender.fulfilled, (state, action) => {
         state.status = "success";
@@ -69,10 +70,12 @@ const goodsSlice = createSlice({
       })
       .addCase(fetchGender.rejected, (state, action) => {
         state.status = "failed";
+        state.totalCount = null;
         state.error = action.error.message;
       })
       .addCase(fetchCategory.pending, (state) => {
         state.status = "loading";
+        state.totalCount = null;
       })
       .addCase(fetchCategory.fulfilled, (state, action) => {
         state.status = "success";
@@ -83,9 +86,11 @@ const goodsSlice = createSlice({
       .addCase(fetchCategory.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+        state.totalCount = null;
       })
       .addCase(fetchAll.pending, (state) => {
         state.status = "loading";
+        state.totalCount = null;
       })
       .addCase(fetchAll.fulfilled, (state, action) => {
         state.status = "success";
@@ -96,6 +101,7 @@ const goodsSlice = createSlice({
       .addCase(fetchAll.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+        state.totalCount = null;
       });
   },
 });
